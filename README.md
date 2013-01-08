5db5347d8552ac1e34ca30d0d4ecb7368f637660
Christian Borkenhagen <christian.borkenhagen@fhtw-berlin.de>
2013-01-08 02:35:26 +0100

    add boost library

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ imagemosaic
 requires: 
 * ImageMagick including development headers and libraries (http://www.imagemagick.org)
 * CMake (http://www.cmake.org/)
+* Boost (http://www.boost.org)
+
+notes:
+* I use boost 1.49 as anything >= 1.50 gives me headaches with the libraries
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,70 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <boost/program_options.hpp>
 #include <Magick++.h>
 
-using namespace std;
+namespace po = boost::program_options;
 
-int main()
-{
-	cout << "Hello World";
+/* imagemosaic supports the following command-line arguments:
+ *
+ * --image-directory, -i : (required) path to directory containing images for creating the mosaic
+ * --verbose, -v         : (optional) enable verbosity
+ * --help, -h            : print usage
+ */
+struct globalArgs_t {
+	const char *image_directory;
+	bool verbose;
+};
+
+po::options_description generic("Generic options");
+po::options_description config("Configuration");
+
+/* print program usage and exit */
+void display_usage(int exit_code) {
+	printf("Usage:\n");
 	
+	exit(exit_code);
+}
+
+/* print program usage when something went wrong */
+void display_usage() {
+	display_usage(EXIT_FAILURE);
+}
+
+/* parse command-line arguments */
+const globalArgs_t parse_arguments(const int argc, char* argv[]) {
+	/*
+	printf("received %i parameters\n", argc);
+	
+	for (int i = 0; i < argc; ++i) {
+		printf("parameter %i: %s\n", i, argv[i]);
+	}
+	*/
+	globalArgs_t globalArgs;
+	
+	// set default values for optional arguments
+	globalArgs.verbose = true;
+		
+	if (globalArgs.verbose) {
+		printf("received the following parameters:\n");
+		printf("\timage-directory: %s\n", globalArgs.image_directory);
+		printf("\tverbose: %s\n", globalArgs.verbose ? "true" : "false");
+	}
+	
+	return globalArgs;
+}
+
+/* MAIN METHDO */
+int main(int argc, char* argv[]) {
+	// parse optional and required program arguments
+	const globalArgs_t args = parse_arguments(argc, argv);
+	
+	// test if setup is really working...
 	Magick::Image image;
 	
-	return 0;
+	// done!
+	if (args.verbose) {
+		puts("exiting program");
+	}
+	exit(EXIT_SUCCESS);
 }
